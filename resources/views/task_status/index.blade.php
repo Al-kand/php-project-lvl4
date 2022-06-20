4f2b2f092641cec4650b310bdf2d71a2041714dd
Alkand <alkand@mysite.com>
2022-06-20 23:23:40 +0300

    restricted guest access

--- resources/views/task_status/index.blade.php
+++ resources/views/task_status/index.blade.php
@@ -4,14 +4,18 @@
     <div class="container">
         @include('flash::message')
         <h1 class="mb-5">{{ __('Statuses') }}</h1>
-        <a href="{{ route('task_statuses.create') }}" class="btn btn-primary">{{ __('Create status') }}</a>
+        @auth
+            <a href="{{ route('task_statuses.create') }}" class="btn btn-primary">{{ __('Create status') }}</a>
+        @endauth
         <table class="table mt-2">
             <thead>
                 <tr>
                     <th>{{ __('ID') }}</th>
                     <th>{{ __('Name') }}</th>
                     <th>{{ __('Created') }}</th>
-                    <th>{{ __('Actions') }}</th>
+                    @auth
+                        <th>{{ __('Actions') }}</th>
+                    @endauth
                 </tr>
             </thead>
             @foreach ($taskStatuses as $taskStatus)
@@ -19,13 +23,15 @@
                     <td>{{ $taskStatus->id }}</td>
                     <td>{{ $taskStatus->name }}</td>
                     <td>{{ $taskStatus->created_at->format('d.m.Y') }}</td>
-                    <td>
-                        <a class="text-danger text-decoration-none"
-                            href="{{ route('task_statuses.destroy', $taskStatus) }}"
-                            data-confirm="{{ __('Are you sure?') }}" data-method="delete">{{ __('Delete') }}</a>
-                        <a class="text-decoration-none"
-                            href="{{ route('task_statuses.edit', $taskStatus) }}">{{ __('Change') }}</a>
-                    </td>
+                    @auth
+                        <td>
+                            <a class="text-danger text-decoration-none"
+                                href="{{ route('task_statuses.destroy', $taskStatus) }}"
+                                data-confirm="{{ __('Are you sure?') }}" data-method="delete">{{ __('Delete') }}</a>
+                            <a class="text-decoration-none"
+                                href="{{ route('task_statuses.edit', $taskStatus) }}">{{ __('Change') }}</a>
+                        </td>
+                    @endauth
                 </tr>
             @endforeach
         </table>
